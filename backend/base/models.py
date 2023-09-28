203178f23842a09368ac14d4ed89b2a0cfcc0d41
PaccyC <abayisengaaimepacifique.com>
2023-09-28 11:02:59 +0200

    First commit

---
+++ backend/base/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Note(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    body=models.TextField(max_length=250,null=True)
